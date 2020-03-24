f23b94d3c76362d01c79b118735866c74708428a
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-24 14:07:26 -0300

    Database config done.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
+const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ Hello: 'World!' });
-});
+app.use(routes);
 
 app.listen(3333);
