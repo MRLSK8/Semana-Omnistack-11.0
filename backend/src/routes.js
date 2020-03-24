b64f865dce4a82fb9d59288a9c13e35365b73bd9
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-24 16:40:44 -0300

    Incidents controller done

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const ongController = require('./controllers/OngController');
+const incidentsController = require('./controllers/IncidentsController');
 const routes = express.Router();
 
+/* ONGs */
+routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
 
-routes.get('/ongs', ongController.index);
+/* Incidents */
+routes.get('/incidents', incidentsController.index);
+routes.post('/incidents', incidentsController.create);
+routes.delete('/incidents/:id', incidentsController.delete);
 
 module.exports = routes;
