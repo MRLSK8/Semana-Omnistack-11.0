25ef4ecb40501512f7bbc07708974be53ea245fe
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-26 18:59:53 -0300

    Header done

--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
-  );
+  return <Routes />;
 }
