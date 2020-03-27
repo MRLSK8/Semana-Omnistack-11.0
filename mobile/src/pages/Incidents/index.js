0017a8f228dcc1593556b1bee4d428bd82b33b5a
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-26 22:05:42 -0300

    Infinit scroll and mobile done.

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -7,18 +7,35 @@ import { Feather } from '@expo/vector-icons';
 import api from '../../services/api';
 
 export default function Incidents() {
-  const navigation = useNavigation();
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const navigation = useNavigation();
 
   function navigationToDetail(incident) {
     navigation.navigate('Details', { incident });
   }
 
   async function loadIncidents() {
-    const response = await api.get('incidents');
-    setIncidents(response.data);
+    if (loading) {
+      return;
+    }
+
+    if (total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get(`incidents?page=${page}`);
+
+    setIncidents([...incidents, ...response.data]);
     setTotal(response.headers['x-total-count']);
+
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -43,7 +60,9 @@ export default function Incidents() {
         style={styles.incidentList}
         data={incidents}
         keyExtractor={incident => String(incident.id)}
-        showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
