edca03de67cdd13add1206d7e453b82ae5d73bb0
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-27 16:14:53 -0300

    Added validation using "celebrate" library.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,19 +5,53 @@ const IncidentsController = require('./controllers/IncidentsController');
 const ongController = require('./controllers/OngController');
 const SessionController = require('./controllers/SessionController');
 
+const { celebrate, Joi, Segments } = require('celebrate');
+
 const routes = express.Router();
 
 /* ONGs */
 routes.get('/ongs', ongController.index);
-routes.post('/ongs', ongController.create);
+routes.post(
+  '/ongs',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number().required(),
+      city: Joi.string().required(),
+      city: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  ongController.create
+);
 
 /* Incidents */
 routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
-routes.delete('/incidents/:id', IncidentsController.delete);
+routes.delete(
+  '/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  IncidentsController.delete
+);
 
 /* Profile */
-routes.get('/profile', ProfileController.index);
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
 
 /* Session */
 routes.post('/sessions', SessionController.create);
